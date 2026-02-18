8f73ae267c8140748afbdf68134c77b3f4e60a5a
khaliqMehmood <khaliq.mehmood@arhamsoft.org>
2026-02-18 17:26:45 +0500

    fix build error

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -73,7 +73,7 @@ function Header() {
             <a href="#contact">Contact Me</a>
           </li>
           <li>
-            <a href="https://calendly.com/danishhameed" target="_blank">Let's Talk</a>
+            <a href="https://calendly.com/danishhameed" target="_blank" rel="noreferrer">Let's Talk</a>
           </li>
           <li>
             {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
